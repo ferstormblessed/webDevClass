d9e50b328110a8eee1f56d4ecd29951f0cf36a32
Fernando Camacho <58267406+FernandoCamacho-hub@users.noreply.github.com>
2022-04-10 02:31:54 -0500

    Add files via upload
    Retro page final

--- RetroWebPage/main.js
+++ RetroWebPage/main.js
@@ -1,17 +1,48 @@
 function writeBook() {
-    var randNum = Math.random() * 5;
+    var randNum = Math.random() * 20;
     randBook = Math.floor(randNum);
 
     var books = ['The Way of Kings',
                  'Words of Radiance',
+                 'Edgedancer',
                  'Oathbringer',
+                 'Dawnshard',
                  'Rhythm of War',
-                 'Mistborn: The Final Empire'];
+                 'Mistborn: The Final Empire',
+                 'The Well of Ascension',
+                 'The Hero of Ages',
+                 'The Alloy of Law',
+                 'Shadows of Self',
+                 'The Bands of Mourning',
+                 'Warbreaker',
+                 'Elantris',
+                 'Whitesand',
+                 'Arcanum Unbounded',
+                 'Skyward',
+                 'Starsight',
+                 'Cytonic',
+                 'The Rithmatist'
+                ];
     var bgImages = ['twok.jpeg',
                     'wor.jpeg',
+                    'edgedancer.jpeg',
                     'oath.jpeg',
+                    'dawnshard.jpeg',
                     'row.jpeg',
-                    'mistborn.jpeg'];
+                    'mistborn.jpeg',
+                    'mistborn2.jpeg',
+                    'mistborn3.jpeg',
+                    'mistborn4.jpeg',
+                    'mistborn5.jpeg',
+                    'mistborn6.jpeg',
+                    'warbreaker.jpeg',
+                    'elantris.jpeg',
+                    'whitesand.jpeg',
+                    'arcanum.jpeg',
+                    'skyward.jpeg',
+                    'starsight.jpeg',
+                    'cytonic.jpeg',
+                    'theRithmatist.jpeg'];
 
     var elem_title = document.getElementById('book-title');
     var elem_bg = document.getElementById('the-bg');
